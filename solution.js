548ad6a7fab6f24618a640de361af375008cafbc
Rahul Prabhu <107515329+rahul-prabhu11@users.noreply.github.com>
2022-06-15 03:34:51 +0530

    Add files via upload

---
+++ solution.js
@@ -0,0 +1,22 @@
+function solution(arr) {
+    let ids = arr.map(item => item.id); // Extracting IDs into an array
+
+    ids = ids.sort((a, b) => { return a - b }) // Sorting the extracted array
+
+    const uniqueArr = [...new Map(arr.map((item) => [item["id"], item])).values()]; // Array of unique objects
+
+    let repeatCount = 0;
+    for (let i = 0; i < ids.length; i++) {
+        if (ids[i + 1] && ids[i] == ids[i + 1])
+            repeatCount++;
+    } // Finding the count of repeated IDs (excluding first count of unique IDs)
+
+    if (repeatCount == 0 || repeatCount >= uniqueArr.length)  // If input array of objects is completely unique or all objects are same
+        return uniqueArr
+
+    let result = [];
+    for (let i = 0; i < (arr.length + repeatCount - uniqueArr.length); i++) { // Compute number of unique set of arrays needed
+        result.push(uniqueArr)
+    }
+    return result;
+}
